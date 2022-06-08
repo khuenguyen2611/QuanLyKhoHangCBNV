712bffb3916b3a408be8443a10530c9018c89c48
khuenguyen98 <68855733+khuenguyen98@users.noreply.github.com>
2022-06-08 16:18:53 +0700

    Add EF 6.x

---
+++ Model/DataProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKhoHangCBNV.Model
+{
+    public class DataProvider
+    {
+        void Demo()
+        {
+            QuanLyKhoHangEntities db = new QuanLyKhoHangEntities();
+            db.Supplies.ToList();
+        }
+    }
+}
