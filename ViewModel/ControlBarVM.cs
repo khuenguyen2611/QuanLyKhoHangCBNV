82a4c858457218b9ceae087f5c092ab4efaadff3
khuenguyen98 <68855733+khuenguyen98@users.noreply.github.com>
2022-06-08 15:34:25 +0700

    Add project files.

---
+++ ViewModel/ControlBarVM.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuanLyKhoHangCBNV.ViewModel
+{
+    public class ControlBarVM : BaseViewModel
+    {
+        #region Commands
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand MaximizeWindowCommand { get; set; }
+        public ICommand MinimizeWindowCommand { get; set; }
+        public ICommand MouseMoveWindowCommand { get; set; }
+        #endregion
+
+        public ControlBarVM()
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) => {
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    w.Close();
+                }
+            }
+            );
+
+            MaximizeWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) => {
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    if (w.WindowState != WindowState.Maximized)
+                        w.WindowState = WindowState.Maximized;
+                    else
+                        w.WindowState = WindowState.Normal;
+                }
+            }
+            );
+
+            MinimizeWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) => {
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    if (w.WindowState != WindowState.Minimized)
+                        w.WindowState = WindowState.Minimized;
+                    else
+                        w.WindowState = WindowState.Maximized;
+                }
+            }
+            );
+
+            MouseMoveWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) => {
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    w.DragMove();
+                }
+            }
+            );
+        }
+
+        FrameworkElement GetWindowParent(UserControl p)
+        {
+            FrameworkElement parent = p;
+
+            while (parent.Parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+            return parent;
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace QuanLyKhoHangCBNV.ViewModel
+{
+    public class MainViewModel
+    {
+        public bool isLoaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+        public ICommand MeasureCommand { get; set; }
+        public ICommand SupplierCommand { get; set; }
+        public ICommand CustomerCommand { get; set; }
+        public ICommand SupplyCommand { get; set; }
+        public ICommand UserCommand { get; set; }
+        public ICommand ImportCommand { get; set; }
+        public ICommand ExportCommand { get; set; }
+
+
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                isLoaded = true;
+
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+            );
+
+            MeasureCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                MeasureWindow measureWindow = new MeasureWindow();
+                measureWindow.ShowDialog();
+            });
+
+            SupplierCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                SupplierWindow supplierWindow = new SupplierWindow();
+                supplierWindow.ShowDialog();
+            }
+            );
+
+            CustomerCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                CustomerWindow customerWindow = new CustomerWindow();
+                customerWindow.ShowDialog();
+            }
+            );
+
+            SupplyCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                SupplyWindow supplyWindow = new SupplyWindow();
+                supplyWindow.ShowDialog();
+            }
+            );
+
+            UserCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                UserWindow userWindow = new UserWindow();
+                userWindow.ShowDialog();
+            }
+            );
+
+            ImportCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                ImportWindow importWindow = new ImportWindow();
+                importWindow.ShowDialog();
+            }
+            );
+
+            ExportCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+                ExportWindow exportWindow = new ExportWindow();
+                exportWindow.ShowDialog();
+            }
+            );
+        }
+    }
+}
